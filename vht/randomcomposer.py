36f358535f8b74581acd5b00580dc0e9b9d5a944
Remigiusz Dybka <remigiusz.dybka@gmail.com>
2018-02-10 05:34:08 +0000

    start with clean mod

--- vht/randomcomposer.py
+++ vht/randomcomposer.py
@@ -10,39 +10,4 @@ def muzakize():
 	mod.bpm = 123
 	
 	seq = mod.add_sequence(32)
-	t1 = seq.add_track()
-	t2 = seq.add_track(0, 1, 12)
-	t3 = seq.add_track(0, 3, 8)
-	
-	t1.channel = t2.channel = 10
-	t3.channel = 2
-	
-	track_fill(t1, "c3", 8, 85)
-	#track_fill(t2, "f#3", 2, 63)
-	#track_fill(t3, "d3", 16, 45)
-	t1.add_column()
-	t1.add_column()
-	
-	t1[0][8].clear()
-	t1[1][8] = "d3"
-	
-	t1[0][24].clear()
-	t1[1][24] = "d3"
-	
-	t1[0][14] = "c3"
-	t1[0][14].velocity = 90
-	
-	t1[0][22] = "c3"
-	t1[0][22].velocity = 90
-	
-	t1[0][28] = "c3"
-	t1[0][28].velocity = 100
-	
 
-#	for i in range(10):
-#		t = seq.add_track()
-#		t.channel = i + 1
-		
-#		for r in range(20):
-#			t[0][r] = "c3"
-#			t[0][r].velocity = 100 + i + 1
